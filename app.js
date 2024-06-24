8301ddbbb441851fca056b8f25674f5c87f99119
CodeAaronM <aaron.martinez.b@gmail.com>
2024-06-24 14:14:24 +0000

    falta-cambio-de-estilos

--- app.js
+++ app.js
@@ -8,13 +8,3 @@ myModal.addEventListener('shown.bs.modal', () => {
 })
 
 
-window.onload = () => {
-  
-    document.querySelector("#random").innerHTML = random();
-  };
-
-  const random = () => {
-    let a = Math.floor(Math.random() * 1000);
-    console.log(a);
-    return a
-}
